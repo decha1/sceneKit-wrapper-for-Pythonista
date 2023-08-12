218ee138b8fb5df3267a0bf222fcea39766c5135
Decha Enkvetchakul <denkvetchakul@yahoo.com>
2023-08-12 02:25:29 -0500

    black formatting applied to all python code

--- sceneKit demo/02_Corvette-F3/Corvette-F3-Demo.py
+++ sceneKit demo/02_Corvette-F3/Corvette-F3-Demo.py
@@ -10,62 +10,68 @@ import math
 
 
 class Demo:
-  def __init__(self):
-    self.name = 'Corvette-F3'
-    pass
-    
-  @classmethod
-  def run(cls):
-    cls().main()
-    
-  @on_main_thread
-  def main(self):
-    main_view = ui.View()
-    w, h = ui.get_screen_size()
-    main_view.frame = (0,0,w,h)
-    main_view.name = 'Corvette-F3 demo'
-  
-    scene_view = scn.View(main_view.frame, superView=main_view)
-    scene_view.autoresizingMask = scn.ViewAutoresizing.FlexibleHeight | scn.ViewAutoresizing.FlexibleWidth
-    scene_view.allowsCameraControl = True
+    def __init__(self):
+        self.name = "Corvette-F3"
+        pass
 
-    scene_view.scene = scn.Scene.sceneWithURL(url='Corvette-F3.scn')
-    
-    root_node = scene_view.scene.rootNode  
-    corvette_node = root_node.childNodes[0]
+    @classmethod
+    def run(cls):
+        cls().main()
 
-    corvette_geometry = corvette_node.geometry
-    corvette_material = corvette_geometry.firstMaterial
+    @on_main_thread
+    def main(self):
+        main_view = ui.View()
+        w, h = ui.get_screen_size()
+        main_view.frame = (0, 0, w, h)
+        main_view.name = "Corvette-F3 demo"
 
-    corvette_material.diffuse.contents = ui.Image.named('SF_Corvette-F3_diffuse.jpg')
-    corvette_material.emission.contents = ui.Image.named('SF_Corvette-F3_glow.jpg')
-    corvette_material.specular.contents = ui.Image.named('SF_Corvette-F3_specular.jpg')
-    corvette_material.normal.contents = ui.Image.named('SF_Corvette-F3_bump.jpg')       
+        scene_view = scn.View(main_view.frame, superView=main_view)
+        scene_view.autoresizingMask = (
+            scn.ViewAutoresizing.FlexibleHeight | scn.ViewAutoresizing.FlexibleWidth
+        )
+        scene_view.allowsCameraControl = True
 
-    constraint = scn.LookAtConstraint.lookAtConstraintWithTarget(corvette_node)
-    constraint.gimbalLockEnabled = True
-    
-    light_node = scn.Node()
-    light_node.position = (-800, -800, -2100)
-    light = scn.Light()
-    light.type = scn.LightTypeDirectional
-    light.castsShadow = True
-    light.color = (.89, .89, .89)
-    light.intensity = 500
-    light_node.light = light
-    light_node.constraints = constraint
-    root_node.addChildNode(light_node)
-    
-    ambient_light = scn.Light() 
-    ambient_light.type = scn.LightTypeAmbient
-    ambient_light.name = 'ambient light'
-    ambient_light.color = (.99, 1.0, .86)
-    ambient_light.intensity = 300
-    ambient_node = scn.Node()
-    ambient_node.light = ambient_light
-    root_node.addChildNode(ambient_node)
-    
+        scene_view.scene = scn.Scene.sceneWithURL(url="Corvette-F3.scn")
 
-    main_view.present(style='fullscreen', hide_title_bar=False)
-    
+        root_node = scene_view.scene.rootNode
+        corvette_node = root_node.childNodes[0]
+
+        corvette_geometry = corvette_node.geometry
+        corvette_material = corvette_geometry.firstMaterial
+
+        corvette_material.diffuse.contents = ui.Image.named(
+            "SF_Corvette-F3_diffuse.jpg"
+        )
+        corvette_material.emission.contents = ui.Image.named("SF_Corvette-F3_glow.jpg")
+        corvette_material.specular.contents = ui.Image.named(
+            "SF_Corvette-F3_specular.jpg"
+        )
+        corvette_material.normal.contents = ui.Image.named("SF_Corvette-F3_bump.jpg")
+
+        constraint = scn.LookAtConstraint.lookAtConstraintWithTarget(corvette_node)
+        constraint.gimbalLockEnabled = True
+
+        light_node = scn.Node()
+        light_node.position = (-800, -800, -2100)
+        light = scn.Light()
+        light.type = scn.LightTypeDirectional
+        light.castsShadow = True
+        light.color = (0.89, 0.89, 0.89)
+        light.intensity = 500
+        light_node.light = light
+        light_node.constraints = constraint
+        root_node.addChildNode(light_node)
+
+        ambient_light = scn.Light()
+        ambient_light.type = scn.LightTypeAmbient
+        ambient_light.name = "ambient light"
+        ambient_light.color = (0.99, 1.0, 0.86)
+        ambient_light.intensity = 300
+        ambient_node = scn.Node()
+        ambient_node.light = ambient_light
+        root_node.addChildNode(ambient_node)
+
+        main_view.present(style="fullscreen", hide_title_bar=False)
+
+
 Demo.run()
--- sceneKit demo/17_cube-puzzle/main.py
+++ sceneKit demo/17_cube-puzzle/main.py
@@ -1,4 +1,4 @@
-'''Cube puzzles - five typical 3x3x3 3D puzzles.
+"""Cube puzzles - five typical 3x3x3 3D puzzles.
 
 (c) Peter Ulbrich, 2019
 
@@ -6,7 +6,7 @@ a demo script for the sceneKit wrapper package for Pythonista
 
 See the README.md for details.
 
-'''
+"""
 
 import sceneKit as scn
 import ui
@@ -18,55 +18,54 @@ import manual
 import selector
 import setup
 
+
 class MainViewController:
-    
-  class MainView(ui.View):
-    def will_close(self):
-      data.gestures_instance.remove_all_gestures(data.main_view)
-      for aSceneView in data.active_stage.scene_views():
-        aSceneView.scene.paused = True
-        aSceneView.removeFromSuperview()
-      for aView in data.main_view.subviews:
-        data.main_view.remove_subview(aView)
-    
-    def layout(self):
-      _, _, w, h = data.main_view.frame
-      data.horizontal = w > h
-      data.hud_layer.layout()
-      data.active_stage.layout()
-      
-    
-  @classmethod
-  def run(cls):
-    scn.clearCache()
-    cls().main()
-  
-  @on_main_thread
-  def main(self):
-    data.main_view = self.MainView()
-    w, h = ui.get_window_size()
-    data.main_view.frame = (0, 0, w, h)
-    
-    data.gestures_instance = Gestures.Gestures()
-    data.gestures_instance.add_tap(data.main_view, self.simple_tap)
+    class MainView(ui.View):
+        def will_close(self):
+            data.gestures_instance.remove_all_gestures(data.main_view)
+            for aSceneView in data.active_stage.scene_views():
+                aSceneView.scene.paused = True
+                aSceneView.removeFromSuperview()
+            for aView in data.main_view.subviews:
+                data.main_view.remove_subview(aView)
 
-    data.hud_layer = Hud()
-    self.next_stage(selector.SelectorStage())
+        def layout(self):
+            _, _, w, h = data.main_view.frame
+            data.horizontal = w > h
+            data.hud_layer.layout()
+            data.active_stage.layout()
 
-    data.main_view.present(style='fullscreen', hide_title_bar=True)
-   
-  @classmethod 
-  def next_stage(cls, stage_instance):
-    if data.active_stage is not None:
-      for aSceneView in data.active_stage.scene_views():
-        aSceneView.scene.paused = True
-        aSceneView.removeFromSuperview()
-    data.active_stage = stage_instance
-    data.hud_layer.bring_to_front()
-    
-  def simple_tap(self, tap_data):
-    data.active_stage.tap(tap_data.location)
-    
+    @classmethod
+    def run(cls):
+        scn.clearCache()
+        cls().main()
 
-if __name__ == '__main__':
-  MainViewController.run()
+    @on_main_thread
+    def main(self):
+        data.main_view = self.MainView()
+        w, h = ui.get_window_size()
+        data.main_view.frame = (0, 0, w, h)
+
+        data.gestures_instance = Gestures.Gestures()
+        data.gestures_instance.add_tap(data.main_view, self.simple_tap)
+
+        data.hud_layer = Hud()
+        self.next_stage(selector.SelectorStage())
+
+        data.main_view.present(style="fullscreen", hide_title_bar=True)
+
+    @classmethod
+    def next_stage(cls, stage_instance):
+        if data.active_stage is not None:
+            for aSceneView in data.active_stage.scene_views():
+                aSceneView.scene.paused = True
+                aSceneView.removeFromSuperview()
+        data.active_stage = stage_instance
+        data.hud_layer.bring_to_front()
+
+    def simple_tap(self, tap_data):
+        data.active_stage.tap(tap_data.location)
+
+
+if __name__ == "__main__":
+    MainViewController.run()
