1c7a6f6357573be53c3cc4a9f30d3c1953a55303
decha-ipad <56658954+decha1@users.noreply.github.com>
2024-07-06 17:38:32 -0500

    fix scenekit import

--- scenekit_demos/avoid_occluder_demo.py
+++ scenekit_demos/avoid_occluder_demo.py
@@ -1,6 +1,12 @@
 """
 avoid occluder demo
 """
+# following block of code is a hack. In pythonista, user modules are reloaded automatically, i.e. there is no need to restart the interpreter. Making sceneKit a user module allows changes to take place when rerunning, as opposed to placing sceneKit in the site-packages folder, which loads only once when the interpreter starts
+import sys, os.path
+sceneKit_directory = os.path.dirname(__file__)
+sceneKit_directory = os.path.join(sceneKit_directory, '..')
+sceneKit_directory = os.path.abspath(sceneKit_directory)
+sys.path.append(sceneKit_directory)
 
 from objc_util import *
 import sceneKit as scn
